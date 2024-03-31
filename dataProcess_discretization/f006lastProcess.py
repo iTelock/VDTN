470edfe526001b442c2376537a192d7c6a1f3fe1
iTelock <35419800+iTelock@users.noreply.github.com>
2024-04-01 00:53:42 +0900

    Update f006lastProcess.py

--- dataProcess_discretization/f006lastProcess.py
+++ dataProcess_discretization/f006lastProcess.py
@@ -1,18 +1,3 @@
-# 接下来帮我写一个python程序f006lastProcess.py，要求如下：
-# 1，使用f005velocityDiscre.py运行后返回的数据bus_data
-# 2，定义变量save_path用于指定保存文件的路径和文件名，默认值为'/Users/mr.tao/mycode/VDTN/deliveryLevel_calculate/x1x6.txt'
-# 3，为bus_data新创建一列，名为deliveryLevel；删除以下列：time，busID，longitude，latitude，velocity
-# 4，将数据存入到save_path中(保存方式：.to_csv(save_path, index=False, header=False, sep=','))
-
-# 我调整了以下f006lastProcess.py的需求，请根据以下要求作修改：
-# 1，使用f005velocityDiscre.py运行后返回的数据bus_data
-# 2，定义变量save_path用于指定保存文件的路径和文件名，默认值为'/Users/mr.tao/mycode/VDTN/deliveryLevel_calculate/x1x6.txt'；定义另一个变量save_path_before也用来指定保存文件的路径和文件名，默认值为'/Users/mr.tao/mycode/VDTN/deliveryLevel_calculate/x1x6All.txt'
-# 3，为bus_data新创建一列，名为deliveryLevel；
-# 4, 调整bus_data列的排序，使其为：regionCode，timeSlot，moveDirection，contactInterval，velocityLevel，pathID，deliveryLevel，time，busID，longitude，latitude，velocity
-# 5，将数据存入到save_path_before中(保存方式：.to_csv(save_path, index=False, header=False, sep=','))
-# 6,删除以下列：time，busID，longitude，latitude，velocity
-# 7,将数据存入到save_path中(保存方式：.to_csv(save_path, index=False, header=False, sep=','))
-
 import pandas as pd
 
 def final_process(bus_data, save_path, save_path_before):
