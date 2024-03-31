c3d8e8148a6f56933fbc350bd5fb5fc625672853
iTelock <35419800+iTelock@users.noreply.github.com>
2024-04-01 00:53:11 +0900

    Update f004contactIntervalDiscre.py

--- dataProcess_discretization/f004contactIntervalDiscre.py
+++ dataProcess_discretization/f004contactIntervalDiscre.py
@@ -1,9 +1,3 @@
-# 接下来帮我写一个python程序f004contactIntervalDiscre.py，要求：
-# 1，使用f003moveDirectionDiscre.py运行后返回的数据bus_data
-# 2，为bus_data新创建一列，名为contactInterval
-# 3，数据中的busID代表不同的车，如果一辆车附近100m范围内有其他车，则称为辆车相遇；现在要计算每辆车和其他车相遇的平均间隔时间，并转化为间隔值，也就是以300秒为粒度，0-300秒视为1,301-600秒视为2……超过3000情况的按照3000秒处理，如果一辆车平均388秒遇到一次其他车辆，则它的间隔值为2。最终将间隔值对应busID存入contactInterval中
-# 4，平均间隔时间可以用某车出现的总时间除以遇到车辆的次数（某一时刻遇到多辆车也算为一次）来计算，总时间是指某车出现的最后时刻和初次出现时刻之间的时间
-
 import pandas as pd
 import numpy as np
 
